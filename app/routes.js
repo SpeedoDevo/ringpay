a390122e477700ad106bfd42c9cac236a80eb455
SpeedoDevo <speedo@devo.hu>
2015-07-03 16:34:14 +0100

    screw twilio

--- app/routes.js
+++ app/routes.js
@@ -5,13 +5,13 @@ var gateway = braintree.connect({
 	publicKey: "y39gvpg9hmz6mw9x",
 	privateKey: "153607c095324e5b4d0a7152240378e2"
 });
-var twilio = require('twilio');
-var client = twilio('AC604badbcc8ce63d0a4a15e1407d28068', 'f51972084f378212d79263923b0e29d8');
-var uid = 0;
+// var twilio = require('twilio');
+// var client = twilio('AC604badbcc8ce63d0a4a15e1407d28068', 'f51972084f378212d79263923b0e29d8');
+// var uid = 0;
 
 module.exports = function(app) {
 
-	// server routes ===========================================================
+// 	// server routes ===========================================================
 	// handle things like api calls
 	// authentication routes
 	app.get('/bt/ctoken',function (req,res){
@@ -30,36 +30,36 @@ module.exports = function(app) {
 		});
 	});
 
-	app.get('/pay', function(req, res){
-		console.log(req.query.uid, req.query.amount);
-		gateway.transaction.sale({
-			customerId: req.query.uid,
-			amount: req.query.amount
-		}, function (err, result) {
-			client.messages.create({
-				to:'+447961223088',
-				from:'+442033897477',
-				body:'Your credit card has been charged £2.50'
-			}, function(error, message) {
-				if (error) {
-					console.log(error.message);
-				}
-			});
-			res.end();
-		});
-	});
+// 	app.get('/pay', function(req, res){
+// 		console.log(req.query.uid, req.query.amount);
+// 		gateway.transaction.sale({
+// 			customerId: req.query.uid,
+// 			amount: req.query.amount
+// 		}, function (err, result) {
+// 			client.messages.create({
+// 				to:'+447961223088',
+// 				from:'+442033897477',
+// 				body:'Your credit card has been charged £2.50'
+// 			}, function(error, message) {
+// 				if (error) {
+// 					console.log(error.message);
+// 				}
+// 			});
+// 			res.end();
+// 		});
+// 	});
 
-	app.get('/uid', function(req, res){
-		// uid = req.body.uid;
-		uid = req.query.uid;
-		res.end(uid);
-	})
+// 	app.get('/uid', function(req, res){
+// 		// uid = req.body.uid;
+// 		uid = req.query.uid;
+// 		res.end(uid);
+// 	})
 
-	// frontend routes =========================================================
-	// route to handle all angular requests
-	app.get('/:uid', function(req, res) {
-		res.sendFile('/public/index.html');
-	});
+// 	// frontend routes =========================================================
+// 	// route to handle all angular requests
+// 	app.get('/:uid', function(req, res) {
+// 		res.sendFile('/public/index.html');
+// 	});
 
 	app.get('*', function(req, res) {
 		res.sendFile('/public/index.html');
